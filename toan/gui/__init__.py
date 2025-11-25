4fed24dcf9f24ee7bd87092d911b8c7c1a1c3f44
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-11-25 01:45:55 -0800

    Add skeleton of capture page

--- toan/gui/__init__.py
+++ toan/gui/__init__.py
@@ -13,6 +13,22 @@ def _play_test_signal() -> None:
     sd.play(signal, THE_SAMPLE_RATE)
 
 
+def _create_debug_tab(notebook: ttk.Notebook) -> ttk.Frame:
+    root = ttk.Frame(notebook)
+    root.grid(column=0, row=0, sticky="NSEW")
+
+    button_test_signal = ttk.Button(
+        root, text="Play Test Signal", command=_play_test_signal
+    )
+    button_test_signal.grid(column=0, row=0, sticky="EW")
+    return root
+
+
+def _create_capture_tab(notebook: ttk.Notebook) -> ttk.Frame:
+    root = ttk.Frame(notebook)
+    return root
+
+
 def run_gui() -> None:
     root = tk.Tk()
     root.geometry("300x300")
@@ -23,15 +39,11 @@ def run_gui() -> None:
 
     notebook = ttk.Notebook(mainframe)
 
-    tab_debug = ttk.Frame(notebook)
-    tab_debug.grid(column=0, row=0, sticky="NSEW")
-
-    button_test_signal = ttk.Button(
-        tab_debug, text="Play Test Signal", command=_play_test_signal
-    )
-    button_test_signal.grid(column=0, row=0, sticky="EW")
-
+    tab_debug = _create_debug_tab(notebook)
     notebook.add(tab_debug, text="Debug")
+
+    tab_capture = _create_capture_tab(notebook)
+    notebook.add(tab_capture, text="Capture")
 
     notebook.pack(fill="both", expand=True)
 
