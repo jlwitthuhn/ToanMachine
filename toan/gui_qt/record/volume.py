14b5bc4ac5f3378eb42a3513595f5640372e28bf
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-12-01 19:51:20 -0800

    Finish volume calibration page

--- toan/gui_qt/record/volume.py
+++ toan/gui_qt/record/volume.py
@@ -4,6 +4,7 @@ import numpy as np
 import sounddevice as sd
 from PySide6 import QtCore, QtWidgets
 
+from toan.generate import generate_tone
 from toan.gui_qt.record import RecordingContext
 
 VOLUME_TEXT = [
@@ -21,9 +22,13 @@ class RecordVolumePage(QtWidgets.QWizardPage):
     play_button: QtWidgets.QPushButton
     play_active: bool = False
 
+    output_callback_tone: np.ndarray
+    output_callback_tone_index: int = 0
+
     bar_input_level: QtWidgets.QProgressBar
     bar_progress: int = 0
     bar_update_timer: QtCore.QTimer
+    text_volume: QtWidgets.QLineEdit
 
     input_stream: sd.InputStream | None = None
     volume_samples: np.ndarray
@@ -34,10 +39,14 @@ class RecordVolumePage(QtWidgets.QWizardPage):
         self.context = context
         self.volume_samples = np.zeros(VOLUME_SAMPLE_COUNT)
 
+        self.output_callback_tone = generate_tone(
+            context.sample_rate, 440.0, 0.8, 20.0, True
+        )
+
         self.bar_update_timer = QtCore.QTimer()
         self.bar_update_timer.setInterval(50)
         self.bar_update_timer.setSingleShot(False)
-        self.bar_update_timer.timeout.connect(self._update_bar)
+        self.bar_update_timer.timeout.connect(self._update_status)
 
         self.setTitle("Set Volume")
         layout = QtWidgets.QVBoxLayout(self)
@@ -63,6 +72,11 @@ class RecordVolumePage(QtWidgets.QWizardPage):
         self.bar_input_level.setMaximum(BAR_PRECISION)
         layout.addWidget(self.bar_input_level)
 
+        self.text_volume = QtWidgets.QLineEdit(self)
+        self.text_volume.setFixedWidth(80)
+        self.text_volume.setReadOnly(True)
+        layout.addWidget(self.text_volume)
+
     def cleanupPage(self, /) -> None:
         if self.play_active:
             self._clicked_play_test()
@@ -80,6 +94,11 @@ class RecordVolumePage(QtWidgets.QWizardPage):
             if self.input_stream is not None:
                 self.input_stream.close()
                 self.input_stream = None
+            if self.output_stream is not None:
+                self.output_stream.close()
+                self.output_stream = None
+            self.bar_progress = 0
+            self._update_status()
             return
         self.play_active = True
         self.play_button.setText("Stop Test Tone")
@@ -87,18 +106,36 @@ class RecordVolumePage(QtWidgets.QWizardPage):
         self._setup_io_streams()
 
     def _setup_io_streams(self):
+        if (
+            self.context.input_channel.device_index
+            == self.context.output_channel.device_index
+        ):
+            raise Exception(
+                "Using the same device for recording and playback is not yet supported."
+            )
         sample_rate = self.context.sample_rate
+
         input_channel = self.context.input_channel
         self.input_stream = sd.InputStream(
             samplerate=sample_rate,
-            blocksize=2048,
+            blocksize=1024,
             device=input_channel.device_index,
             callback=self._input_callback,
         )
         self.input_stream.start()
 
-    def _update_bar(self):
+        output_channel = self.context.output_channel
+        self.output_stream = sd.OutputStream(
+            samplerate=sample_rate,
+            blocksize=1024,
+            device=output_channel.device_index,
+            callback=self._output_callback,
+        )
+        self.output_stream.start()
+
+    def _update_status(self):
         self.bar_input_level.setValue(self.bar_progress)
+        self.text_volume.setText(f"{self.bar_progress / 10}")
 
     def _input_callback(
         self, indata: np.ndarray, frames: int, time, status: sd.CallbackFlags
@@ -110,3 +147,18 @@ class RecordVolumePage(QtWidgets.QWizardPage):
         ) % VOLUME_SAMPLE_COUNT
         out_val = self.volume_samples.max()
         self.bar_progress = min(BAR_PRECISION, math.floor(out_val * 1000))
+
+    def _output_callback(
+        self, outdata: np.ndarray, frames: int, time, status: sd.CallbackFlags
+    ) -> None:
+        outdata.fill(0)
+
+        if self.output_callback_tone_index + frames >= len(self.output_callback_tone):
+            self.output_callback_tone_index = 0
+        segment = self.output_callback_tone[
+            self.output_callback_tone_index : self.output_callback_tone_index + frames
+        ]
+        self.output_callback_tone_index += frames
+
+        channel = self.context.output_channel.channel_index - 1
+        outdata[:, channel] = segment
