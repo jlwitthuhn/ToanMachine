b8b702972a304347b7e987eaea373d4938e7f780
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-12-01 23:14:46 -0800

    Add non-functional wet signal recording page

--- toan/gui_qt/record/__init__.py
+++ toan/gui_qt/record/__init__.py
@@ -4,6 +4,7 @@ from toan.gui_qt.record.context import RecordingContext
 from toan.gui_qt.record.device import RecordDevicePage
 from toan.gui_qt.record.intro import RecordIntroPage
 from toan.gui_qt.record.volume import RecordVolumePage
+from toan.gui_qt.record.wet import RecordWetSignalPage
 
 
 class RecordWizard(QtWidgets.QWizard):
@@ -16,6 +17,7 @@ class RecordWizard(QtWidgets.QWizard):
         self.addPage(RecordIntroPage(self))
         self.addPage(RecordDevicePage(self, self.context))
         self.addPage(RecordVolumePage(self, self.context))
+        self.addPage(RecordWetSignalPage(self, self.context))
 
         self.setWindowTitle("Recording Wizard")
         self.setModal(True)
---
+++ toan/gui_qt/record/wet.py
@@ -0,0 +1,37 @@
+from PySide6 import QtWidgets
+
+from toan.gui_qt.record import RecordingContext
+
+RECORD_TEXT = [
+    "In this section you will send a signal through your pedal and record the result.",
+    "Once you have started, allow the full recording to complete before proceeding. Do not change any settings on your pedal while recording.",
+]
+
+
+class RecordWetSignalPage(QtWidgets.QWizardPage):
+    def __init__(self, parent, context: RecordingContext):
+        super().__init__(parent)
+
+        self.setTitle("Record")
+        layout = QtWidgets.QVBoxLayout(self)
+
+        label = QtWidgets.QLabel("\n\n".join(RECORD_TEXT), self)
+        label.setWordWrap(True)
+        layout.addWidget(label)
+
+        hline = QtWidgets.QFrame(self)
+        hline.setFrameShape(QtWidgets.QFrame.Shape.HLine)
+        hline.setFrameShadow(QtWidgets.QFrame.Shadow.Sunken)
+        layout.addWidget(hline)
+
+        button_record = QtWidgets.QPushButton("Record")
+        layout.addWidget(button_record)
+
+        label_progress = QtWidgets.QLabel("Progress:", self)
+        layout.addWidget(label_progress)
+
+        progress_bar = QtWidgets.QProgressBar(self)
+        layout.addWidget(progress_bar)
+
+    def isComplete(self, /):
+        return False
