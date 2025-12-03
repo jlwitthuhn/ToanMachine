b4350ea4dc05f27d12fa3a2591aa85440809f678
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-12-02 23:29:04 -0800

    Remove unused import

--- toan/gui/record/wet.py
+++ toan/gui/record/wet.py
@@ -1,7 +1,6 @@
 # This file is part of Toan Machine and is licensed under the GPLv3
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 # SPDX-License-Identifier: GPL-3.0-only
-from multiprocessing.spawn import prepare
 
 import numpy as np
 import sounddevice as sd
