ec34ebbe111f12cb0e6bdf660ef3227538d488a7
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-12-02 22:21:47 -0800

    Fix typo in license header

--- toan/gui/record/context.py
+++ toan/gui/record/context.py
@@ -1,6 +1,6 @@
 # This file is part of Toan Machine and is licensed under the GPLv3
 # https://www.gnu.org/licenses/gpl-3.0.en.html
-# SPDX-License-Identified: GPL-3.0-only
+# SPDX-License-Identifier: GPL-3.0-only
 
 from toan.soundio import SdChannel
 
