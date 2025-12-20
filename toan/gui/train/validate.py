6afb4f4c538739bff66aa5f5daa3771829b2f342
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2025-12-19 20:51:36 -0800

    Only allow advancing once validation is complete

--- toan/gui/train/validate.py
+++ toan/gui/train/validate.py
@@ -15,13 +15,15 @@ from toan.gui.train import TrainingContext
 
 
 class _ValidateThreadContext:
-    text_edit: QtWidgets.QTextEdit
+    def __init__(self, page):
+        self.page = page
 
 
 class TrainValidatePage(QtWidgets.QWizardPage):
     context: TrainingContext
     thread_context: _ValidateThreadContext | None = None
     text_edit: QtWidgets.QTextEdit
+    validated: bool = False
 
     def __init__(self, parent, context: TrainingContext):
         super().__init__(parent)
@@ -42,7 +44,7 @@ class TrainValidatePage(QtWidgets.QWizardPage):
     def initializePage(self):
         self.text_edit.clear()
         self.text_edit.append(f"Analyzing input file: {self.context.input_path}")
-        self.thread_context = _ValidateThreadContext()
+        self.thread_context = _ValidateThreadContext(self)
         self.thread_context.text_edit = self.text_edit
 
         def thread_func():
@@ -50,10 +52,12 @@ class TrainValidatePage(QtWidgets.QWizardPage):
 
         threading.Thread(target=thread_func).start()
 
+    def isComplete(self):
+        return self.validated
+
 
 def _find_clicks(signal: np.ndarray, raw_noise_floor: float) -> list[int]:
     noise_threshold = raw_noise_floor * 3.5
-    print("noise_threshold", noise_threshold)
     click_indices = []
     silence_samples_remaining = 0
     for i in range(len(signal)):
@@ -68,97 +72,82 @@ def _find_clicks(signal: np.ndarray, raw_noise_floor: float) -> list[int]:
 
 
 def _run_thread(context: _ValidateThreadContext, input_path: str):
-    context.text_edit.append("Loading as zip archive...")
+    def print_status(message: str):
+        context.page.text_edit.append(message)
+
+    print_status("Loading as zip archive...")
     try:
         with zipfile.ZipFile(input_path, "r") as zip_file:
-            context.text_edit.append("Loading config...")
+            print_status("Loading config...")
 
             try:
                 config_json_bytes = zip_file.read("config.json")
                 config_json = json.loads(config_json_bytes.decode("utf-8"))
             except KeyError:
-                context.text_edit.append("Error: config.json not found")
+                print_status("Error: config.json not found")
                 return
 
             if "version" not in config_json:
-                context.text_edit.append(
-                    "Error: config.json does not contain key 'version'"
-                )
+                print_status("Error: config.json does not contain key 'version'")
                 return
             if (
                 not isinstance(config_json["version"], int)
                 or config_json["version"] != 0
             ):
-                context.text_edit.append("Error: config.json has unknown version")
+                print_status("Error: config.json has unknown version")
                 return
 
             if "device_name" not in config_json or not isinstance(
                 config_json["device_name"], str
             ):
-                context.text_edit.append(
-                    "Error: config.json does not contain key 'device_name'"
-                )
+                print_status("Error: config.json does not contain key 'device_name'")
                 return
 
-            context.text_edit.append(f"Device name: {config_json["device_name"]}")
+            print_status(f"Device name: {config_json["device_name"]}")
 
             if "sample_rate" not in config_json or not isinstance(
                 config_json["sample_rate"], int
             ):
-                context.text_edit.append(
-                    "Error: config.json does not contain key 'sample_rate'"
-                )
+                print_status("Error: config.json does not contain key 'sample_rate'")
                 return
 
-            context.text_edit.append(f"Sample rate: {config_json["sample_rate"]}")
+            print_status(f"Sample rate: {config_json["sample_rate"]}")
 
             if "dry_signal" not in config_json or not isinstance(
                 config_json["dry_signal"], str
             ):
-                context.text_edit.append(
-                    "Error: config.json does not contain key 'dry_signal'"
-                )
+                print_status("Error: config.json does not contain key 'dry_signal'")
                 return
             if "wet_signal" not in config_json or not isinstance(
                 config_json["wet_signal"], str
             ):
-                context.text_edit.append(
-                    "Error: config.json does not contain key 'wet_signal'"
-                )
+                print_status("Error: config.json does not contain key 'wet_signal'")
                 return
 
             try:
-                context.text_edit.append(
-                    f"Loading dry signal: {config_json["dry_signal"]}"
-                )
+                print_status(f"Loading dry signal: {config_json["dry_signal"]}")
                 dry_signal_bytes = zip_file.read(config_json["dry_signal"])
                 with io.BytesIO(dry_signal_bytes) as dry_bytes_io:
                     dry_sample_rate, dry_signal = wavfile.read(dry_bytes_io)
                     if dry_sample_rate != config_json["sample_rate"]:
-                        context.text_edit.append(
-                            "Error: dry signal has unexpected sample rate"
-                        )
+                        print_status("Error: dry signal has unexpected sample rate")
                         return
             except:
-                context.text_edit.append(
+                print_status(
                     f"Error: Failed to load dry signal from {config_json["dry_signal"]}"
                 )
                 return
 
             try:
-                context.text_edit.append(
-                    f"Loading wet signal: {config_json["wet_signal"]}"
-                )
+                print_status(f"Loading wet signal: {config_json["wet_signal"]}")
                 wet_signal_bytes = zip_file.read(config_json["wet_signal"])
                 with io.BytesIO(wet_signal_bytes) as wet_bytes_io:
                     wet_sample_rate, wet_signal = wavfile.read(wet_bytes_io)
                     if wet_sample_rate != config_json["sample_rate"]:
-                        context.text_edit.append(
-                            "Error: wet signal has unexpected sample rate"
-                        )
+                        print_status("Error: wet signal has unexpected sample rate")
                         return
             except:
-                context.text_edit.append(
+                print_status(
                     f"Error: Failed to load wet signal from {config_json["wet_signal"]}"
                 )
                 return
@@ -167,25 +156,21 @@ def _run_thread(context: _ValidateThreadContext, input_path: str):
             dry_silent = dry_signal[dry_sample_rate // 8 : 3 * dry_sample_rate // 8]
             dry_clicks = dry_signal[3 * dry_sample_rate // 8 : 5 * dry_sample_rate // 4]
             dry_noise_floor = np.max(np.abs(dry_silent))
-            context.text_edit.append(f"Dry noise floor: {dry_noise_floor}")
+            print_status(f"Dry noise floor: {dry_noise_floor}")
 
             wet_silent = wet_signal[wet_sample_rate // 8 : 3 * wet_sample_rate // 8]
             wet_clicks = wet_signal[3 * wet_sample_rate // 8 : 5 * wet_sample_rate // 4]
             wet_noise_floor = np.max(np.abs(wet_silent))
-            context.text_edit.append(f"Wet noise floor: {wet_noise_floor}")
+            print_status(f"Wet noise floor: {wet_noise_floor}")
 
             dry_click_indices = _find_clicks(dry_clicks, wet_noise_floor)
             if len(dry_click_indices) != 2:
-                context.text_edit.append(
-                    f"Error: Failed to locate clicks in dry signal"
-                )
+                print_status(f"Error: Failed to locate clicks in dry signal")
                 return
 
             wet_click_indices = _find_clicks(wet_clicks, wet_noise_floor)
             if len(wet_click_indices) != 2:
-                context.text_edit.append(
-                    f"Error: Failed to locate clicks in wet signal"
-                )
+                print_status(f"Error: Failed to locate clicks in wet signal")
                 return
 
             # Click delta is the time between two clicks on a single track
@@ -193,20 +178,23 @@ def _run_thread(context: _ValidateThreadContext, input_path: str):
             dry_click_delta = dry_click_indices[1] - dry_click_indices[0]
             wet_click_delta = wet_click_indices[1] - wet_click_indices[0]
             click_delta_delta = abs(dry_click_delta - wet_click_delta)
-            context.text_edit.append(f"Click delta delta: {click_delta_delta}")
+            print_status(f"Click delta delta: {click_delta_delta}")
 
             if click_delta_delta > 4:
-                context.text_edit.append("Error: Click delta delta is greater than 4")
+                print_status("Error: Click delta delta is greater than 4")
                 return
 
             latency_samples_a = wet_click_indices[0] - dry_click_indices[0]
             latency_samples_b = wet_click_indices[1] - dry_click_indices[1]
             latency_samples = min(latency_samples_a, latency_samples_b)
-            context.text_edit.append(f"Recording latency: {latency_samples} samples")
+            print_status(f"Recording latency: {latency_samples} samples")
+
+            context.page.validated = True
+            context.page.completeChanged.emit()
 
     except zipfile.BadZipFile:
-        context.text_edit.append("Error: File is not a valid zip archive")
+        print_status("Error: File is not a valid zip archive")
         return
     except:
-        context.text_edit.append("Error: Unknown error occurred")
+        print_status("Error: Unknown error occurred")
         return
