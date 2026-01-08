7cbd5f79f2cff4520bdd1ef9392d9384ec20fcb5
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-07 20:35:42 -0800

    Add missing newline

--- toan/gui/train/train.py
+++ toan/gui/train/train.py
@@ -1,6 +1,7 @@
 # This file is part of Toan Machine and is licensed under the GPLv3
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 # SPDX-License-Identifier: GPL-3.0-only
+
 import threading
 from dataclasses import dataclass
 
