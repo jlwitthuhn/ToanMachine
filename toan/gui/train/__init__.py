b312d94155a2fe15670722250e6a56c41193829a
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-09 18:07:47 -0800

    Add empty graph page after training

--- toan/gui/train/__init__.py
+++ toan/gui/train/__init__.py
@@ -5,6 +5,7 @@
 from PySide6 import QtCore, QtWidgets
 
 from toan.gui.train.context import TrainingContext
+from toan.gui.train.graph import TrainGraphPage
 from toan.gui.train.input import TrainInputFilePage
 from toan.gui.train.intro import TrainIntroPage
 from toan.gui.train.train import TrainTrainPage
@@ -23,6 +24,7 @@ class TrainingWizard(QtWidgets.QWizard):
         self.addPage(TrainInputFilePage(self, self.context))
         self.addPage(TrainValidatePage(self, self.context))
         self.addPage(TrainTrainPage(self, self.context))
+        self.addPage(TrainGraphPage(self, self.context))
 
         self.setWindowTitle("Training Wizard")
         self.setModal(True)
---
+++ toan/gui/train/graph.py
@@ -0,0 +1,21 @@
+# This file is part of Toan Machine and is licensed under the GPLv3
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+# SPDX-License-Identifier: GPL-3.0-only
+
+from PySide6 import QtWidgets
+
+from toan.gui.train import TrainingContext
+
+
+class TrainGraphPage(QtWidgets.QWizardPage):
+    context: TrainingContext
+
+    def __init__(self, parent, context: TrainingContext):
+        super().__init__(parent)
+        self.context = context
+
+        self.setTitle("Training Graphs")
+        layout = QtWidgets.QVBoxLayout(self)
+
+        label = QtWidgets.QLabel("Training Loss")
+        layout.addWidget(label)
