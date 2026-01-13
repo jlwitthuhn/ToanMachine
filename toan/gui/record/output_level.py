0daec513da488c8e445cabab8372e69aaa05660f
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-13 00:07:36 -0800

    Almost enough to record a test signal on output level page

--- toan/gui/record/output_level.py
+++ toan/gui/record/output_level.py
@@ -3,9 +3,13 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 import numpy as np
+import sounddevice as sd
 from PySide6 import QtWidgets
 
 from toan.gui.record import RecordingContext
+from toan.music import get_note_frequency_by_name
+from toan.signal.chord import generate_generic_chord_pluck
+from toan.soundio import SdPlayrecController, prepare_play_record
 
 OUTPUT_LEVEL_TEXT = [
     "In this section you will set the output level of your interface. For overdrive and similar effects it is very important to get this right to ensure that the input signal is loud enough to trigger the desired clipping effect.",
@@ -20,8 +24,13 @@ class RecordOutputLevelPage(QtWidgets.QWizardPage):
     button_record: QtWidgets.QPushButton
     button_play: QtWidgets.QPushButton
 
+    generated_chords: np.ndarray | None = None
+    generated_chord_index: int = 0
+
     recorded_buffer: np.ndarray | None = None
-    recording: bool = False
+    recorded_buffer_partial: np.ndarray | None = None
+
+    io_controller: SdPlayrecController | None = None
 
     def __init__(self, parent, context: RecordingContext):
         super().__init__(parent)
@@ -57,23 +66,48 @@ class RecordOutputLevelPage(QtWidgets.QWizardPage):
         self._refresh_buttons()
 
     def _pressed_record(self):
-        if self.recording:
+        if self.recorded_buffer_partial is not None:
             return
 
-        self.recording = True
         self.recorded_buffer = None
+        self.recorded_buffer_partial = np.ndarray([0])
         self._refresh_buttons()
+
+        if self.generated_chords is None:
+            d_root = get_note_frequency_by_name("D", 3, 440.0)
+            self.generated_chords = generate_generic_chord_pluck(
+                self.context.sample_rate, [4, 7], d_root, 2.0
+            )
+        self.generated_chord_index = 0
+
+        self.io_controller = prepare_play_record(
+            self.context.sample_rate,
+            self.context.input_channel,
+            self.context.output_channel,
+            self._record_input_callback,
+            self._record_output_callback,
+        )
+
+    def _record_input_callback(
+        self, data: np.ndarray, frames: int, time, status: sd.CallbackFlags
+    ):
+        pass
+
+    def _record_output_callback(
+        self, data: np.ndarray, frames: int, time, status: sd.CallbackFlags
+    ):
+        data.fill(0)
 
     def _refresh_buttons(self):
         play_enabled = True
         record_enabled = True
 
-        if self.recording:
-            play_enabled = False
-            record_enabled = False
-
         if self.recorded_buffer is None:
             play_enabled = False
+
+        if self.recorded_buffer_partial is not None:
+            play_enabled = False
+            record_enabled = False
 
         self.button_play.setEnabled(play_enabled)
         self.button_record.setEnabled(record_enabled)
