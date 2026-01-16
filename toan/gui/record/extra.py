b34bc138bb68616ebb0fbf51f87cd00dffa3d7d2
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-15 23:21:40 -0800

    Add non-functional checkbox to extra audio page

--- toan/gui/record/extra.py
+++ toan/gui/record/extra.py
@@ -5,6 +5,8 @@
 from PySide6 import QtWidgets
 
 from toan.gui.record import RecordingContext
+from toan.persistence import get_user_wav_list
+from toan.qt import WavFileModel
 
 EXTRA_AUDIO_TEXT = [
     "Select any extra audio you would like to include in the recorded signal.",
@@ -14,6 +16,8 @@ EXTRA_AUDIO_TEXT = [
 
 class RecordExtraPage(QtWidgets.QWizardPage):
     context: RecordingContext
+
+    table: QtWidgets.QTableView
 
     def __init__(self, parent, context: RecordingContext):
         super().__init__(parent)
@@ -25,3 +29,11 @@ class RecordExtraPage(QtWidgets.QWizardPage):
         label = QtWidgets.QLabel("\n\n".join(EXTRA_AUDIO_TEXT), self)
         label.setWordWrap(True)
         layout.addWidget(label)
+
+        self.table = QtWidgets.QTableView(self)
+        layout.addWidget(self.table)
+
+    def initializePage(self):
+        wav_files = get_user_wav_list()
+        model = WavFileModel(self, wav_files, True)
+        self.table.setModel(model)
--- toan/qt/__init__.py
+++ toan/qt/__init__.py
@@ -10,28 +10,45 @@ from toan.persistence import UserWavDesc
 
 class WavFileModel(QtCore.QAbstractTableModel):
     file_list: list[UserWavDesc]
+    with_checkbox: bool
+    selected_wavs: set[UserWavDesc]
 
-    def __init__(self, parent, file_list: list[UserWavDesc]):
+    def __init__(
+        self, parent, file_list: list[UserWavDesc], with_checkbox: bool = False
+    ):
         super().__init__(parent)
         self.file_list = file_list
+        self.with_checkbox = with_checkbox
 
     def rowCount(self, parent=QtCore.QModelIndex()):
         return len(self.file_list)
 
     def columnCount(self, parent=QtCore.QModelIndex()):
-        return 3
+        if self.with_checkbox:
+            return 4
+        else:
+            return 3
 
     def data(self, index, /, role=...):
         if not index.isValid():
             return None
-        if role != Qt.DisplayRole:
-            return None
-        if index.column() == 0:
-            return self.file_list[index.row()].filename
-        if index.column() == 1:
-            return f"{self.file_list[index.row()].sample_rate}"
-        if index.column() == 2:
-            return f"{self.file_list[index.row()].duration:.2f}s"
+
+        if self.with_checkbox:
+            offset = 1
+        else:
+            offset = 0
+
+        if role == Qt.DisplayRole:
+            if index.column() == 0 + offset:
+                return self.file_list[index.row()].filename
+            if index.column() == 1 + offset:
+                return f"{self.file_list[index.row()].sample_rate}"
+            if index.column() == 2 + offset:
+                return f"{self.file_list[index.row()].duration:.2f}s"
+
+        if role == Qt.CheckStateRole and self.with_checkbox and index.column() == 0:
+            return Qt.Unchecked
+
         return None
 
     def headerData(self, section, orientation, /, role=...):
@@ -39,10 +56,24 @@ class WavFileModel(QtCore.QAbstractTableModel):
             return None
         if orientation != Qt.Horizontal:
             return None
-        if section == 0:
+
+        if self.with_checkbox:
+            offset = 1
+        else:
+            offset = 0
+
+        if self.with_checkbox and section == 0:
+            return "Enabled"
+        if section == 0 + offset:
             return "Filename"
-        if section == 1:
+        if section == 1 + offset:
             return "Samp. Rate"
-        if section == 2:
+        if section == 2 + offset:
             return "Duration"
         return None
+
+    def flags(self, index):
+        flags = super().flags(index)
+        if self.with_checkbox and index.column() == 0:
+            flags |= Qt.ItemIsUserCheckable | Qt.ItemIsEnabled
+        return flags
