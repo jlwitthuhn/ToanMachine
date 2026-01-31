aae9445e9b1fea71db478d52d4851a7a714cf595
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-01-31 03:59:52 -0800

    Avoid generating warble pitches beyond the nyquist frequency

--- toan/signal/warble.py
+++ toan/signal/warble.py
@@ -14,6 +14,12 @@ BIG_CYCLES_PER_SECOND = 3.0
 PHASE_CYCLES_PER_SECOND = 4.0
 MAXIMUM_PHASE_OFFSET = math.pi / 2.0
 
+WARBLE_PITCH_BIG_MIN = 0.75
+WARBLE_PITCH_BIG_MAX = 1.333
+
+WARBLE_PITCH_SMALL_MIN = 0.99
+WARBLE_PITCH_SMALL_MAX = 1.01
+
 
 def _generate_signal_from_frequencies(
     sample_rate: int, frequencies: np.ndarray, phase_offset: float
@@ -31,7 +37,10 @@ def _generate_tone_warble(
     frequencies.fill(frequency)
     frequencies *= modulation
     frequency_multiplier = generate_sine_wave(
-        sample_count, int(sample_rate / PHASE_CYCLES_PER_SECOND), 0.99, 1.01
+        sample_count,
+        int(sample_rate / PHASE_CYCLES_PER_SECOND),
+        WARBLE_PITCH_SMALL_MIN,
+        WARBLE_PITCH_SMALL_MAX,
     )
     frequencies *= frequency_multiplier
     return _generate_signal_from_frequencies(sample_rate, frequencies, phase_offset)
@@ -40,7 +49,10 @@ def _generate_tone_warble(
 def _generate_top_level_modulation(sample_rate: int, duration: float) -> np.ndarray:
     sample_count = int(sample_rate * duration)
     return generate_sine_wave(
-        sample_count, int(sample_rate / BIG_CYCLES_PER_SECOND), 0.75, 1.333
+        sample_count,
+        int(sample_rate / BIG_CYCLES_PER_SECOND),
+        WARBLE_PITCH_BIG_MIN,
+        WARBLE_PITCH_BIG_MAX,
     )
 
 
@@ -67,6 +79,9 @@ def generate_warble_chord(
             frequency = increase_frequency_by_semitones(
                 root_frequency, note + 12 * octave_index
             )
+            if frequency * WARBLE_PITCH_BIG_MAX >= sample_rate // 2:
+                # Something something nyquist frequency
+                break
             note_signal = _generate_tone_warble(
                 sample_rate, modulation, frequency, phase
             )
