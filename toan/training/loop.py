0f5a85401f8f753da1f85243e37286ff9ccf1699
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-02-01 23:33:14 -0800

    Move more stuff out of training stage loop

--- toan/training/loop.py
+++ toan/training/loop.py
@@ -2,22 +2,56 @@
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 # SPDX-License-Identifier: GPL-3.0-only
 
+from typing import Any
+
 from mlx import core as mx
 from mlx import nn as nn
 from mlx import optimizers as optimizers
 
 from toan.model.nam_wavenet import NamWaveNet
 from toan.training import LossFunction, TrainingSummary
-from toan.training.config import TrainingConfig
+from toan.training.config import TrainingConfig, TrainingStageConfig
 from toan.training.context import TrainingProgressContext
 from toan.training.data_loader import TrainingDataLoader
 
 
 def run_training_loop(context: TrainingProgressContext, config: TrainingConfig):
+    assert len(config.stages) > 0
     mx.random.seed(0o35)
     model = NamWaveNet(context.model_config, context.metadata, context.sample_rate)
     assert model is not None
     mx.eval(model.parameters())
+
+    def get_batch_size(stage_cfg: TrainingStageConfig, iter: int) -> int:
+        if stage_cfg.batch_size > 0:
+            return stage_cfg.batch_size
+        assert len(stage_cfg.batch_size_list) > 0
+        progress = iter / stage_cfg.steps_total()
+        result = 1
+        for threshold, count in stage_cfg.batch_size_list:
+            if progress >= threshold:
+                result = count
+            else:
+                break
+        return result
+
+    def get_learn_rate(stage_cfg: TrainingStageConfig) -> Any:
+        decay_lr = optimizers.cosine_decay(
+            stage_cfg.learn_rate_hi,
+            stage_cfg.steps_main,
+            stage_cfg.learn_rate_lo,
+        )
+        if stage_config.steps_warmup > 0:
+            warmup_lr = optimizers.linear_schedule(
+                stage_cfg.learn_rate_hi / 100.0,
+                stage_cfg.learn_rate_hi,
+                stage_cfg.steps_warmup,
+            )
+            return optimizers.join_schedules(
+                [warmup_lr, decay_lr], [stage_config.steps_warmup]
+            )
+        else:
+            return decay_lr
 
     def get_test_data() -> tuple[mx.array, mx.array]:
         input = mx.array(context.signal_dry_test).reshape((1, -1))
@@ -31,26 +65,17 @@ def run_training_loop(context: TrainingProgressContext, config: TrainingConfig):
         test_in, test_out = get_test_data()
         return model.loss(test_in, test_out, func).item()
 
+    with context.lock:
+        context.iters_done = 0
+        context.iters_total = config.steps_total()
+        context.loss_train = 1.0
+        context.loss_test = 1.0
+
     for stage_config in config.stages:
         summary = TrainingSummary(test_interval=stage_config.test_interval)
         context.summary = summary
 
-        decay_lr = optimizers.cosine_decay(
-            stage_config.learn_rate_hi,
-            stage_config.steps_main,
-            stage_config.learn_rate_lo,
-        )
-        if stage_config.steps_warmup > 0:
-            warmup_lr = optimizers.linear_schedule(
-                stage_config.learn_rate_hi / 100.0,
-                stage_config.learn_rate_hi,
-                stage_config.steps_warmup,
-            )
-            learn_rate = optimizers.join_schedules(
-                [warmup_lr, decay_lr], [stage_config.steps_warmup]
-            )
-        else:
-            learn_rate = decay_lr
+        learn_rate = get_learn_rate(stage_config)
 
         data_loader = TrainingDataLoader(
             context.signal_dry_train,
@@ -68,33 +93,14 @@ def run_training_loop(context: TrainingProgressContext, config: TrainingConfig):
             learning_rate=learn_rate, weight_decay=stage_config.weight_decay
         )
 
-        with context.lock:
-            context.iters_done = 0
-            context.iters_total = stage_config.steps_total()
-            context.loss_train = 1.0
-            context.loss_test = 1.0
-
-        train_loss_buffer = mx.ones(16)
+        train_loss_buffer = mx.ones(12)
         train_loss_buffer_sz = len(train_loss_buffer)
-
-        def get_batch_size(iter: int) -> int:
-            if stage_config.batch_size > 0:
-                return stage_config.batch_size
-            assert len(stage_config.batch_size_list) > 0
-            progress = iter / stage_config.steps_total()
-            result = 1
-            for threshold, count in stage_config.batch_size_list:
-                if progress >= threshold:
-                    result = count
-                else:
-                    break
-            return result
 
         for i in range(stage_config.steps_total()):
             if context.quit:
                 return
             model.train(True)
-            this_batch_size = get_batch_size(i)
+            this_batch_size = get_batch_size(stage_config, i)
             batch_in, batch_out = data_loader.make_batch(this_batch_size)
             loss, grads = loss_and_grad_fn(model, batch_in, batch_out)
             optimizer.update(model, grads)
