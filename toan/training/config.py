25827002c742b971f13cc20f9a769ea3f36738c8
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-02-05 18:30:30 -0800

    Update default training config, use MSE loss instead of RMSE

--- toan/training/config.py
+++ toan/training/config.py
@@ -9,25 +9,19 @@ from toan.training import LossFunction
 
 @dataclass
 class TrainingStageConfig:
-    steps_warmup: int = 40
-    steps_main: int = 460
-    test_interval: int = 25
+    steps_warmup: int = 100
+    steps_main: int = 1100
+    test_interval: int = 50
     # If batch_size is 0, batch_size_list will be used
     batch_size: int = 0
     batch_size_list: list[tuple[float, int]] = field(
-        default_factory=lambda: [
-            (0.0, 24),
-            (0.1, 32),
-            (0.8, 40),
-            (0.9, 48),
-            (0.98, 64),
-        ]
+        default_factory=lambda: [(0.0, 24), (0.4, 40), (0.8, 56), (0.95, 72)]
     )
     input_sample_width: int = 8192 + 4096
-    learn_rate_hi: float = 5.0e-3
-    learn_rate_lo: float = 3.0e-3
+    learn_rate_hi: float = 3.0e-3
+    learn_rate_lo: float = 5.0e-3
     weight_decay: float = 1.1e-2
-    loss_fn: LossFunction = LossFunction.RMSE
+    loss_fn: LossFunction = LossFunction.MSE
     adam_betas: list[float] = field(default_factory=lambda: [0.89, 0.98])
 
     def steps_total(self) -> int:
