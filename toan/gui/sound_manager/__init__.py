fe057e604f561ef5b0e92340c00a8b0e35f818be
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-03-18 02:33:45 -0700

    Add GUI to import wav files

--- toan/gui/sound_manager/__init__.py
+++ toan/gui/sound_manager/__init__.py
@@ -1,10 +1,13 @@
 # This file is part of Toan Machine and is licensed under the GPLv3
 # https://www.gnu.org/licenses/gpl-3.0.en.html
 # SPDX-License-Identifier: GPL-3.0-only
+import os
+from pathlib import Path
 
 from PySide6 import QtWidgets
+from scipy.io import wavfile
 
-from toan.persistence import UserWavDesc, get_user_wav_list
+from toan.persistence import UserWavDesc, get_user_wav_dir, get_user_wav_list
 from toan.qt import WavFileModel
 
 SOUND_TEXT = [
@@ -14,6 +17,8 @@ SOUND_TEXT = [
 
 
 class SoundManager(QtWidgets.QDialog):
+    the_table: QtWidgets.QTableView | None = None
+
     def __init__(self, parent):
         super().__init__(parent)
 
@@ -31,10 +36,43 @@ class SoundManager(QtWidgets.QDialog):
         wav_files: list[UserWavDesc] = get_user_wav_list()
         wav_file_model = WavFileModel(self, wav_files)
 
-        table = QtWidgets.QTableView(self)
-        table.setModel(wav_file_model)
-        table.resizeColumnsToContents()
-        layout.addWidget(table)
+        self.the_table = QtWidgets.QTableView(self)
+        self.the_table.setModel(wav_file_model)
+        self.the_table.resizeColumnsToContents()
+        layout.addWidget(self.the_table)
 
         refresh_button = QtWidgets.QPushButton("Refresh")
+        refresh_button.clicked.connect(self._pressed_refresh)
         layout.addWidget(refresh_button)
+
+        import_button = QtWidgets.QPushButton("Import wav...")
+        import_button.clicked.connect(self._pressed_import)
+        layout.addWidget(import_button)
+
+    def _pressed_import(self):
+        path_str, _ = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Import WAV file", filter="*.wav"
+        )
+        if path_str == "":
+            return
+
+        path = Path(path_str)
+        if not path.is_file():
+            return
+
+        sample_rate, signal = wavfile.read(path)
+
+        out_dir = get_user_wav_dir()
+        out_file = path.name
+        if not out_file.endswith(".wav"):
+            out_file += ".wav"
+        out_path = os.path.join(out_dir, out_file)
+
+        wavfile.write(out_path, sample_rate, signal)
+
+        self._pressed_refresh()
+
+    def _pressed_refresh(self):
+        wav_files: list[UserWavDesc] = get_user_wav_list()
+        wav_file_model = WavFileModel(self, wav_files)
+        self.the_table.setModel(wav_file_model)
