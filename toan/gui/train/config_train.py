962d12412e3170c60a0f78543ae1fbd4e5509cc8
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-04-08 20:59:24 -0700

    Allow customizing training schedule

--- toan/gui/train/config_train.py
+++ toan/gui/train/config_train.py
@@ -28,11 +28,13 @@ class TrainTrainConfigPage(QtWidgets.QWizardPage):
         layout = QtWidgets.QVBoxLayout(self)
 
         self.radio_default = QtWidgets.QRadioButton("Default training configuration")
+        self.radio_default.toggled.connect(lambda: self.edit_disable())
         layout.addWidget(self.radio_default)
 
         layout.addSpacing(8)
 
         radio_custom = QtWidgets.QRadioButton("Custom training configuration")
+        radio_custom.toggled.connect(lambda: self.edit_enable())
         layout.addWidget(radio_custom)
 
         form_widget = QtWidgets.QWidget(self)
@@ -62,17 +64,42 @@ class TrainTrainConfigPage(QtWidgets.QWizardPage):
         the_stage = self.context.train_config.stages[0]
         self.radio_default.setChecked(True)
 
-        self.edit_warmup_steps.setReadOnly(True)
         self.edit_warmup_steps.setText(str(the_stage.steps_warmup))
-
-        self.edit_main_steps.setReadOnly(True)
         self.edit_main_steps.setText(str(the_stage.steps_main))
-
-        self.edit_input_width.setReadOnly(True)
         self.edit_input_width.setText(str(the_stage.input_sample_width))
-
-        self.edit_lr_hi.setReadOnly(True)
         self.edit_lr_hi.setText(str(the_stage.learn_rate_hi))
-
-        self.edit_lr_lo.setReadOnly(True)
         self.edit_lr_lo.setText(str(the_stage.learn_rate_lo))
+        self.edit_disable()
+
+    def validatePage(self):
+        if not self.radio_default.isChecked():
+            try:
+                new_warmup_steps = int(self.edit_warmup_steps.text())
+                new_main_steps = int(self.edit_main_steps.text())
+                new_input_width = int(self.edit_input_width.text())
+                new_lr_hi = float(self.edit_lr_hi.text())
+                new_lr_lo = float(self.edit_lr_lo.text())
+                the_stage = self.context.train_config.stages[0]
+                the_stage.steps_warmup = new_warmup_steps
+                the_stage.steps_main = new_main_steps
+                the_stage.steps_input_width = new_input_width
+                the_stage.learn_rate_hi = new_lr_hi
+                the_stage.learn_rate_lo = new_lr_lo
+                return True
+            except ValueError:
+                return False
+        return True
+
+    def edit_enable(self):
+        self.edit_warmup_steps.setReadOnly(False)
+        self.edit_main_steps.setReadOnly(False)
+        self.edit_input_width.setReadOnly(False)
+        self.edit_lr_hi.setReadOnly(False)
+        self.edit_lr_lo.setReadOnly(False)
+
+    def edit_disable(self):
+        self.edit_warmup_steps.setReadOnly(True)
+        self.edit_main_steps.setReadOnly(True)
+        self.edit_input_width.setReadOnly(True)
+        self.edit_lr_hi.setReadOnly(True)
+        self.edit_lr_lo.setReadOnly(True)
