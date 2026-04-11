92ad2cee18fa7023e1ddbfd9ba373e5b7c6ad311
Jeff <5405091+jlwitthuhn@users.noreply.github.com>
2026-04-11 16:31:47 -0700

    Use gaussian multiplier to avoid harsh transients at the end of chirps

--- toan/signal/generator/chirp.py
+++ toan/signal/generator/chirp.py
@@ -4,16 +4,26 @@
 
 import numpy as np
 
+from toan.signal.generator.gaussian import generate_gaussian_pulse
+
 
 def generate_chirp(
-    sample_rate: float,
+    sample_rate: int,
     begin_freq: float,
     end_freq: float,
     duration: float,
+    blend_ending: int = None,
 ) -> np.ndarray:
-    f = np.logspace(
-        np.log10(begin_freq), np.log10(end_freq), int(duration * sample_rate), False
-    )
+    if blend_ending is None:
+        blend_ending = sample_rate // 1000
+
+    sample_duration = int(sample_rate * duration)
+    assert blend_ending <= sample_duration
+    f = np.logspace(np.log10(begin_freq), np.log10(end_freq), sample_duration, False)
     f_rad = f * 2 * np.pi
     phase = np.cumsum(f_rad) / sample_rate
-    return np.sin(phase)
+    result = np.sin(phase)
+    if blend_ending > 0:
+        end_mult = generate_gaussian_pulse(blend_ending * 2, 0)[-blend_ending:]
+        result[-blend_ending:] = end_mult * result[-blend_ending:]
+    return result
